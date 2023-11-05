381e23a4cc55e43d18fd03aef58a06ae7f7c84d3
Baris Cem Baykara <cem@eleport.net>
2023-11-05 21:54:33 +0200

    Refactor

--- src/Models.h
+++ src/Models.h
@@ -2,34 +2,31 @@
 #ifndef MODELS_H
 #define MODELS_H
 
-struct LedData
-{
-    int r;
-    int g;
-    int b;
-    int cw;
-    int cc;
+struct LedData {
+  int r;
+  int g;
+  int b;
+  int cw;
+  int cc;
 };
 
-enum class Color
-{
-    R = 0,
-    B = 1,
-    G = 2,
-    CW = 3,
-    CC = 4
-};
+enum class Color { R = 0, B = 1, G = 2, CW = 3, CC = 4 };
 
-String getColorString(Color color)
-{
-    switch (color) {
-        case Color::R: return "R";
-        case Color::G: return "G";
-        case Color::B: return "B";
-        case Color::CW: return "CW";
-        case Color::CC: return "CC";
-        default: return "Unknown";
-    }
+String getColorString(Color color) {
+  switch (color) {
+    case Color::R:
+      return "R";
+    case Color::G:
+      return "G";
+    case Color::B:
+      return "B";
+    case Color::CW:
+      return "CW";
+    case Color::CC:
+      return "CC";
+    default:
+      return "Unknown";
+  }
 }
 
 #endif
